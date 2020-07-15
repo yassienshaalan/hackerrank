76c18513e3820f2a9c4296960fe646c5e0c44aad
yassien <yassein@growingdata.com.au>
2020-07-16 09:31:14 +1000

    adding max-sum array solution dynamic programming

---
+++ Dynamic Programming/README.md
@@ -0,0 +1,7 @@
+
+##### This directory contains solutions to the following problems:
+#### 1. [Max Array Sum](https://www.hackerrank.com/challenges/max-array-sum/problem)
+#### 
+
+
+###### The code implemented here is using Python 3.7 
---
+++ Dynamic Programming/max_sum_array.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#######################################
+####   Problem description link #
+#https://www.hackerrank.com/challenges/max-array-sum/problem
+#######################################
+
+def maxSubsetSum(arr):
+    incl = 0
+    excl = 0
+    for i in range(len(arr)): 
+        # Current max excluding i (No ternary in  
+        # Python) 
+        print("before","inc",incl,"exc",excl)
+        new_excl = max(excl,incl)
+         
+        # Current max including i 
+        incl = excl + arr[i] 
+        excl = new_excl 
+        print("after","inc",incl,"exc",excl)
+      
+    # return max of incl and excl 
+    return max(excl,incl) 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = maxSubsetSum(arr)
+
+    fptr.write(str(res) + '\n')
+
+    fptr.close()
